e65d5a3004f9ddc8ea97f45bdcbb400c4e554d77
fpadilha2 <flavio.padilha@dxc.com>
2020-02-19 15:17:24 -0300

    Executar Servidores

--- File Log Form/Form1.Designer.cs
+++ File Log Form/Form1.Designer.cs
@@ -109,6 +109,8 @@
             // 
             // progressBar1
             // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.progressBar1.Location = new System.Drawing.Point(112, 333);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(809, 23);
