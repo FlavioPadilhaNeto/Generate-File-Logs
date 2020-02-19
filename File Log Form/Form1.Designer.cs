d4d0795bbc9550711f4a73f42c7b3156291ed2a0
fpadilha2 <flavio.padilha@dxc.com>
2020-02-19 09:24:29 -0300

    versâo windows form

---
+++ File Log Form/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace File_Log_Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtCaminho = new System.Windows.Forms.TextBox();
+            this.btnAbrirPasta = new System.Windows.Forms.Button();
+            this.listResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(14, 63);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 0;
+            this.btnSalvar.Text = "Iniciar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // txtCaminho
+            // 
+            this.txtCaminho.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtCaminho.Location = new System.Drawing.Point(112, 18);
+            this.txtCaminho.Name = "txtCaminho";
+            this.txtCaminho.Size = new System.Drawing.Size(809, 20);
+            this.txtCaminho.TabIndex = 1;
+            // 
+            // btnAbrirPasta
+            // 
+            this.btnAbrirPasta.Location = new System.Drawing.Point(14, 15);
+            this.btnAbrirPasta.Name = "btnAbrirPasta";
+            this.btnAbrirPasta.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrirPasta.TabIndex = 2;
+            this.btnAbrirPasta.Text = "Abrir Pasta";
+            this.btnAbrirPasta.UseVisualStyleBackColor = true;
+            this.btnAbrirPasta.Click += new System.EventHandler(this.btnAbrirPasta_Click);
+            // 
+            // listResult
+            // 
+            this.listResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listResult.FormattingEnabled = true;
+            this.listResult.Location = new System.Drawing.Point(112, 63);
+            this.listResult.Name = "listResult";
+            this.listResult.Size = new System.Drawing.Size(809, 290);
+            this.listResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 366);
+            this.Controls.Add(this.listResult);
+            this.Controls.Add(this.btnAbrirPasta);
+            this.Controls.Add(this.txtCaminho);
+            this.Controls.Add(this.btnSalvar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtCaminho;
+        private System.Windows.Forms.Button btnAbrirPasta;
+        private System.Windows.Forms.ListBox listResult;
+    }
+}
+
---
+++ File Log Form/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Log_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrirPasta_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtCaminho.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+       
+            listResult.Items.Add("Selecione a pasta onde estâo localizados os LOG´s.");
+        
+            Count = 0;
+
+            var selectedPath = txtCaminho.Text;
+
+            if (System.IO.Directory.Exists(selectedPath)) 
+            {
+                var mainFolder = selectedPath.Length + 1;
+                var retorno = getFiles(new System.IO.DirectoryInfo(selectedPath), mainFolder);
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var item in retorno)
+                {
+                    var fullLine = String.Join("|", item.columns) + "|" + item.Size + "|" + item.valido + "|" + item.Tipo;
+                    listResult.Items.Add(fullLine);
+                    sb.AppendLine(fullLine);
+                }
+
+                listResult.Items.Add("Relatório gerado com sucesso com base em " + Count + " arquivos, agora selecione uma pasta para salvar o CSV.");
+
+                FolderBrowserDialog opSalvar = new FolderBrowserDialog();
+
+                if (opSalvar.ShowDialog() == DialogResult.OK)
+                {
+                    if (System.IO.File.Exists(opSalvar.SelectedPath + "\\relatorio.csv"))
+                        System.IO.File.AppendAllText(opSalvar.SelectedPath + "\\relatorio.csv", sb.ToString());
+                    else
+                        System.IO.File.WriteAllText(opSalvar.SelectedPath + "\\relatorio.csv", sb.ToString());
+                }
+
+                listResult.Items.Add("Relatório Salvo no caminho: " + opSalvar.SelectedPath + "\\relatorio.csv");
+           
+            }
+        }
+
+        public static int Count { get; set; }
+
+      
+        static IEnumerable<Arquivos> getFiles(DirectoryInfo directoryInfo, int mainFolder)
+        {
+            List<Arquivos> retorno = new List<Arquivos>();
+
+            var subDirectory = directoryInfo.GetDirectories();
+
+            if (subDirectory.Length > 0)
+            {
+                foreach (var item in subDirectory)
+                {
+                    retorno.AddRange(getFiles(item, mainFolder));
+                }
+            }
+
+            var files = directoryInfo.GetFiles();
+
+            if (files.Length > 0)
+            {
+                foreach (var item in files)
+                {
+                    var regex = @"[0-9]{1}	[0-9]{2}\/[0-9]{2}\/[0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}	[A-Za-z0-9]{1}";
+                    var regexError = "sistema	versao	local	tipo	ult_msg	DatHorIni	DatHor_ult_msg	dathor_ult_email";
+
+                    Arquivos arquivo = new Arquivos(item.FullName.Split('\\').Last(), item.Length);
+                    arquivo.columns = item.FullName.Substring(mainFolder).Split('\\');
+                    using (StreamReader sw = new StreamReader(item.FullName))
+                    {
+                        string line = sw.ReadLine();
+
+                        if (Regex.IsMatch(line, regex))
+                            arquivo.Tipo = "Log";
+                        else if (line.Equals(regexError))
+                            arquivo.Tipo = "Erro";
+
+                        arquivo.valido = Regex.IsMatch(line, regex) || line.Equals(regexError);
+                        arquivo.FirstLine = line;
+                    }
+
+                    Count++;
+
+                    retorno.Add(arquivo);
+                }
+            }
+
+            return retorno;
+        }
+    }
+
+    public class Arquivos
+    {
+        public Arquivos(string _File, long _Size)
+        {
+            this.File = _File;
+            this.Size = _Size;
+        }
+        public string FirstLine { get; set; }
+        public long Size { get; set; }
+        public IEnumerable<string> columns { get; set; }
+        public string File { get; set; }
+        public string Tipo { get; set; }
+        public bool valido { get; set; }
+    }
+}
